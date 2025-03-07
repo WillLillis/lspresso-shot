8560864dd7431e77a986addfef3b9900499a3dab
WillLillis <will.lillis24@gmail.com>
2025-03-06 21:41:37 -0500

    fix: allow empty responses to diagnostics requests

--- lspresso-shot/lua_templates/diagnostic_autocmd.lua
+++ lspresso-shot/lua_templates/diagnostic_autocmd.lua
@@ -3,9 +3,13 @@ local progress_count = 0 -- track how many times we've tried for the logs
 vim.api.nvim_create_autocmd('DiagnosticChanged', {
     callback = function(_)
         progress_count = progress_count + 1
-        report_log('Issuing hover request (Attempt ' .. tostring(progress_count) .. ')\n') ---@diagnostic disable-line: undefined-global
+        if progress_count < PROGRESS_THRESHOLD then ---@diagnostic disable-line: undefined-global
+            report_log(tostring(progress_count) .. ' < ' .. tostring(PROGRESS_THRESHOLD) .. '\n') ---@diagnostic disable-line: undefined-global
+            return
+        end
+        report_log('Issuing diagnostic request (Attempt ' .. tostring(progress_count) .. ')\n') ---@diagnostic disable-line: undefined-global
         local diagnostics_result = vim.diagnostic.get(0, {})
-        if diagnostics_result and #diagnostics_result >= 1 then
+        if diagnostics_result then
             local results_file = io.open('RESULTS_FILE', 'w')
             if not results_file then
                 report_error('Could not open results file') ---@diagnostic disable-line: undefined-global
