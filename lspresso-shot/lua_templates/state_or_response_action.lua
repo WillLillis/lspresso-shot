813df377dd43a860505426cad155e1a9c00ea0c3
Will Lillis <will.lillis24@gmail.com>
2025-05-23 06:15:56 -0400

    fix(lua): genericize formatting lua file to support other invoke style requests

--- lspresso-shot/lua_templates/state_or_response_action.lua
+++ lspresso-shot/lua_templates/state_or_response_action.lua
@@ -9,39 +9,30 @@ local function check_progress_result()
     end
 
     local params = {}
+    ---@diagnostic disable-next-line: undefined-global, exp-in-action
+    PARAM_ASSIGN
 
-    report_log('Issuing formatting request (Attempt ' .. tostring(progress_count) .. ')\n') ---@diagnostic disable-line: undefined-global
+    report_log('Params: ' .. tostring(vim.inspect(params)) .. '\n') ---@diagnostic disable-line: undefined-global
+    report_log('Testing REQUEST_METHOD (Attempt ' .. tostring(progress_count) .. ')\n') ---@diagnostic disable-line: undefined-global
 
     if INVOKE_FORMAT then ---@diagnostic disable-line: undefined-global
+        report_log('Invoking') ---@diagnostic disable-line: undefined-global
         local results_file = io.open('RESULTS_FILE', 'w')
         if not results_file then
             report_error('Could not open results file') ---@diagnostic disable-line: undefined-global
             exit() ---@diagnostic disable-line: undefined-global
         end
-        ---@diagnostic disable-next-line: undefined-global, exp-in-action
-        PARAM_ASSIGN
 
-        report_log('Format opts: ' .. tostring(vim.inspect(params)) .. '\n') ---@diagnostic disable-line: undefined-global
-        vim.lsp.buf.format({
-            async = false,
-            formatting_options = params.formatting_options,
-        })
+        INVOKE_FN(params) ---@diagnostic disable-line: undefined-global
         local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
-        local formatted = table.concat(lines, "\\n")
+        local final_state = table.concat(lines, "\\n")
         ---@diagnostic disable: need-check-nil
-        results_file:write('"' .. formatted .. '"')
+        results_file:write('"' .. final_state .. '"')
         results_file:close()
         ---@diagnostic enable: need-check-nil
     else
-        ---@diagnostic disable-next-line: undefined-global, exp-in-action
-        PARAM_ASSIGN
-
-        report_log('Format opts: ' .. tostring(vim.inspect(params)) .. '\n') ---@diagnostic disable-line: undefined-global
-
-        local formatting_result = vim.lsp.buf_request_sync(0, 'textDocument/formatting', {
-            textDocument = vim.lsp.util.make_text_document_params(0),
-            options = params.options
-        })
+        report_log('Requesting') ---@diagnostic disable-line: undefined-global
+        local formatting_result = vim.lsp.buf_request_sync(0, 'REQUEST_METHOD', params)
         if not formatting_result then
             ---@diagnostic disable-next-line: undefined-global
             report_log('No valid formatting result returned: ' .. vim.inspect(formatting_result) .. '\n')
